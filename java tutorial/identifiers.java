b7502dfc4fc58e09e0171854cdf4fdf02b714609
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-07 22:49:39 +0530

    Add files via upload

---
+++ java tutorial/identifiers.java
@@ -0,0 +1,10 @@
+public class identifiers{
+    public static void main(String[]args){
+        String name = "yathi";
+        int id = 5252;
+        char grade = 'A';
+        System.out.println("Student name"+name);
+        System.out.println("Student id" + id);
+        System.out.println("Student grade" +grade);
+    }
+}
