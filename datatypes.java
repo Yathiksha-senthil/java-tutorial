0c717feb5c08fa0124ca413e16ee4f15202f7b53
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-12 20:41:41 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,8 @@
+public class datatypes{
+    public static void main(String[]args){
+boolean isjavafun = true;
+boolean isjavascript = false;
+System.out.println(isjavafun);
+System.out.println(isjavascript);
+    }
+}
