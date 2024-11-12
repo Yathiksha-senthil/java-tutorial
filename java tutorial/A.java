7b8bed3a97029cce6eb78b9acc9e1f05afcf859d
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-12 20:44:33 +0530

    Add files via upload

---
+++ java tutorial/A.java
@@ -0,0 +1,8 @@
+public class A {
+    public static void main(String[]args){
+        int NoOfStudents = 50;
+        float Studentpercentage = 99.2f;
+        System.out.println("NoOfStudents" +NoOfStudents);
+        System.out.println("Studentpercentage" +Studentpercentage);
+    }
+}
