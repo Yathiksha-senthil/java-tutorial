27c0cd2cd7e019d643d4d353dd748c64089f4f99
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-12 20:45:14 +0530

    Add files via upload

---
+++ java tutorial/B.java
@@ -0,0 +1,8 @@
+public class B{
+    public static void main(String[]args){
+    String name="Yathi";
+    String name2="Yazhinee";
+    System.out.println("name:" +name);
+    System.out.println("name:" +name2);
+    }
+}
