80d575041a040cd9c5849ed2503f190032273f34
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-12 20:49:25 +0530

    Delete B.java

--- B.java
+++
@@ -1,8 +0,0 @@
-public class B{
-    public static void main(String[]args){
-    String name="Yathi";
-    String name2="Yazhinee";
-    System.out.println("name:" +name);
-    System.out.println("name:" +name2);
-    }
-}
