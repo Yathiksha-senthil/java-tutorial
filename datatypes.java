ffa6c85d2ff9df7c14cdc53d11dee88a6541164e
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-11-12 20:49:46 +0530

    Delete datatypes.java

--- datatypes.java
+++
@@ -1,8 +0,0 @@
-public class datatypes{
-    public static void main(String[]args){
-boolean isjavafun = true;
-boolean isjavascript = false;
-System.out.println(isjavafun);
-System.out.println(isjavascript);
-    }
-}
