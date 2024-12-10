e6c0502ddb1101d8db1ce0d9e23580462130f213
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-12-10 23:04:13 +0530

    Add files via upload

---
+++ java tutorial/Cricket.java
@@ -0,0 +1,13 @@
+public class Cricket{
+    public static void main(String args[]){
+        boolean cricket=false;
+        boolean football=false;
+        if(cricket||football)
+        {
+            System.out.print("Play");
+        }
+        else{
+            System.out.print("Not play");
+        }
+    }
+}
