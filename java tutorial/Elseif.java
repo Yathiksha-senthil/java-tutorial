431adac0f62480a1669b1d843ede1fb76fc433ba
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-12-10 23:02:11 +0530

    Add files via upload

---
+++ java tutorial/Elseif.java
@@ -0,0 +1,13 @@
+public class Elseif{
+    public static void main(String args[]){
+        int mark=49;
+        if(mark>35 && mark<50)
+        {
+            System.out.print("I get video game");
+        }
+        else if(mark>60 && mark<70)
+        {
+            System.out.print("I get Iphone");
+        }
+    }
+}
