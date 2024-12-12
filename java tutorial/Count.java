cabee47414e9b5c3e5954d13e2d66e222d499885
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-12-12 12:48:35 +0530

    Add files via upload

---
+++ java tutorial/Count.java
@@ -0,0 +1,14 @@
+class Count{
+    public static void main(String args[]){
+        int oddcount=0;
+        for(int i=1;i<=10;i=i+1)
+        if(i % 2 == 0)
+        {
+            //System.out.println(Even number:"+i);
+        }else{
+            oddcount=oddcount+1;
+        
+        }
+        System.out.print(oddcount);
+    }
+}
