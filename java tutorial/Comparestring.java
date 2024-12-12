2c9e6c0d14c5ade63274b924b66a2d36fa71087b
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-12-12 12:52:13 +0530

    Add files via upload

---
+++ java tutorial/Comparestring.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+public class Comparestring{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String match = scan.nextLine();
+        if(RCB.equals("win"));
+        {
+            System.out.print("ee sala cup namadhey");
+        }
+        else{
+            System.out.println("no cup");
+        }
+    }
+}
---
+++ java tutorial/Ternary.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Ternary{
+    public static void main(String args[]){
+        boolean rain = false;
+        String result =(rain)?"Take an umbrella":"Enjoy the sunlight";
+        System.out.print(result);
+    }
+}
