fd1370bc8135b9558dc32663f0134c24eef9fc0d
Yathiksha-senthil <yathikshasenthil@gmail.com>
2024-12-12 12:50:31 +0530

    Add files via upload

---
+++ java tutorial/Integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Integer{
+    public static void main(String args[]){
+Scanner scan = new Scanner(System.in);
+int a = scan.nextInt();
+int b = scan.nextInt();
+String result=(a>b)?"True":"False";
+System.out.print(result);
+    }
+}
